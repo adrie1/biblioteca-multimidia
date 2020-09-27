9938803ca43eb12f9788bab00d002ccf84d2feeb
Adriele Costa <44513370+adrie1@users.noreply.github.com>
2020-09-27 15:50:52 -0300

    Projeto Criado

---
+++ src/main/java/br/com/universidade/bibliotecamultimidia/BibliotecaMultimidiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.universidade.bibliotecamultimidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaMultimidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaMultimidiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/universidade/bibliotecamultimidia/BibliotecaMultimidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.universidade.bibliotecamultimidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaMultimidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
